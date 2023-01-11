d0e2d0114319bc326cceea28bee7ba77e405418b
BarzaGuignol <yayav.yaya@gmail.com>
2023-01-11 17:14:40 +0100

    ajout d'un bouton qui mène au Fun le vrai

---
+++ frontend/src/components/ButtonToFun.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./ButtonToFun.css";
+
+export default function ButtonToFun() {
+  return (
+    <div className="bgColor">
+      <button className="noselect blue" type="button">
+        Way To Fun
+      </button>
+    </div>
+  );
+}
