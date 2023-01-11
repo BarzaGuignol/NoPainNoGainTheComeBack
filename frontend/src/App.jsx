752001412c2576365656453e48a3bfb5905d18f0
JoyMarkarian <jmarkarian@hotmail.fr>
2023-01-11 22:20:06 +0100

    Création de la page AllVehicles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import AddVehicle from "@pages/addVehicle";
+import AllVehicles from "@pages/AllVehicles";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
@@ -11,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/vehicle/creation" element={<AddVehicle />} />
+          <Route path="/vehicles" element={<AllVehicles />} />
         </Routes>
       </div>
     </Router>
