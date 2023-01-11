507a9eef0698ff9b4ec611fb0d7005dd7ed3e297
BarzaGuignol <yayav.yaya@gmail.com>
2023-01-11 18:56:45 +0100

    mise en place des saisies dates et type vehicules

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Booking from "@pages/Booking";
 import Home from "./pages/Home";
 
 import "./App.css";
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Home />
+      <Booking />
     </div>
   );
 }
---
+++ frontend/src/pages/Booking.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+export default function Booking() {
+  const [decisionDepart, setDecisionDepart] = useState("2023-01-11");
+  const [decisionArrival, setDecisionArrival] = useState("2023-01-11");
+
+  const handleDecisionDeparture = (e) => {
+    setDecisionDepart(e.target.value);
+  };
+
+  const handleDecisionArrival = (e) => {
+    setDecisionArrival(e.target.value);
+  };
+
+  return (
+    <div className="text-center">
+      <h1 className="text-green-900 font-bold mb-20 text-2xl underline">
+        Booking
+      </h1>
+      <div className="border-solid border-2 border-green-900 w-4/5 mx-auto p-8">
+        <form>
+          <label htmlFor="departure" className="mr-8">
+            Departure :
+          </label>
+          <input
+            className="border-2 border-500 rounded-lg outline-[#c8c8c8]"
+            type="date"
+            id="departure"
+            name="trip-departure"
+            value={decisionDepart}
+            onChange={handleDecisionDeparture}
+          />
+        </form>
+
+        <form>
+          <label htmlFor="departure" className="mr-8">
+            Arrival :
+          </label>
+          <input
+            className="border-2 border-500 rounded-lg outline-[#c8c8c8]"
+            type="date"
+            id="departure"
+            name="trip-departure"
+            value={decisionArrival}
+            onChange={handleDecisionArrival}
+          />
+        </form>
+
+        <form className="mb-20">
+          <label htmlFor="selectVehicle" className="mr-8">
+            Select type of vehicle :
+          </label>
+          <select id="selectVehicle">
+            <option value="break">Break</option>
+            <option value="sedan">Sedan</option>
+            <option value="SUV">SUV</option>
+            <option value="Limousine">Limousine</option>
+            <option value="truck">Truck</option>
+            <option value="pick-up">Pick-up</option>
+            <option value="van">Van</option>
+          </select>
+        </form>
+        <button
+          className="border-solid border-2 border-green-900 rounded-full w-40 text-green-900"
+          type="button"
+        >
+          Validation
+        </button>
+      </div>
+    </div>
+  );
+}
