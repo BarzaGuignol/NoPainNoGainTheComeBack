2ff6be11646a795d26a4276efb794937a6df1315
BarzaGuignol <yayav.yaya@gmail.com>
2023-01-12 01:43:25 +0100

    finition footer route way to fun fonctionnelle

--- frontend/src/pages/PanicRoom.jsx
+++ frontend/src/pages/PanicRoom.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+import CoffeeMachine from "@components/CoffeeMachine";
+
 export default function PanicRoom() {
-  return <div>Please don't panic !</div>;
+  return (
+    <div className="bg-slate-500 h-screen">
+      <h3 className="text-center pt-20 text-red-500 font-bold text-2xl">
+        Please don't panic !
+      </h3>
+      <CoffeeMachine />
+    </div>
+  );
 }
