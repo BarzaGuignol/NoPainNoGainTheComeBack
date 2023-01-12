390b97f83b7707f77be274f53285ed85262f8f65
BarzaGuignol <yayav.yaya@gmail.com>
2023-01-12 17:18:09 +0100

    retouche hauteur footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="h-96 overflow-hidden flex flex-col items-center justify-center bg-[#333]">
+    <div className="h-48 overflow-hidden flex flex-col items-center justify-center bg-[#333]">
       <h3 className="mb-8 text-white">
         Copyright NoPainNoGainTheComeBack © 2023 - All rights reserved
       </h3>
