4783563a4a9d18e532733a6e5d83030c74ca8c5f
o6ris <loic.tsiry@gmail.com>
2023-01-13 01:06:18 +0100

    hot fixes

--- frontend/src/pages/SummaryBook.jsx
+++ frontend/src/pages/SummaryBook.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Nav from "@components/Nav";
 import apiConnexion from "../services/apiConnexion";
 
 function SummaryBook() {
@@ -24,27 +25,30 @@ function SummaryBook() {
   const arrival = new Date(booking.arrival_date);
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="mt-20 flex flex-col items-center border-black">
-        <h2 className="mt-5 font-medium mx-3">
-          Thank you {booking.firstname} {booking.lastname} for your booking !
-        </h2>
-        <hr className="mt-3 mx-3" />
-        <span className="mt-5 mx-3 font-normal">
-          Reservation number : #00{id}
-        </span>
-        <div className="grid grid-cols-2 gap-2 mx-1 mx-0">
+    <>
+      <Nav />
+      <div className="flex flex-col items-center h-[80vh]">
+        <div className="mt-20 flex flex-col items-center border-black">
+          <h2 className="mt-5 font-medium mx-3">
+            Thank you {booking.firstname} {booking.lastname} for your booking !
+          </h2>
+          <hr className="mt-3 mx-3" />
           <span className="mt-5 mx-3 font-normal">
-            Departure date : {`${departure.toLocaleDateString()}`}
+            Reservation number : #00{id}
           </span>
-          <span className="mt-5 mx-3 font-normal">
-            Arrival date : {`${arrival.toLocaleDateString()}`}
-          </span>
+          <div className="grid grid-cols-2 gap-2 mx-1 mx-0">
+            <span className="mt-5 mx-3 font-normal">
+              Departure date : {`${departure.toLocaleDateString()}`}
+            </span>
+            <span className="mt-5 mx-3 font-normal">
+              Arrival date : {`${arrival.toLocaleDateString()}`}
+            </span>
+          </div>
+          <span className="mt-5 mx-3 font-normal">{booking.model}</span>
+          <img className="w-40" src={booking.picture} alt={booking.model} />
         </div>
-        <span className="mt-5 mx-3 font-normal">{booking.model}</span>
-        <img className="w-40" src={booking.picture} alt={booking.model} />
       </div>
-    </div>
+    </>
   );
 }
 
