b82df74ecd46f5323e6fb9427e6659ab88e378f2
o6ris <loic.tsiry@gmail.com>
2023-01-13 02:00:14 +0100

    fix de css

--- frontend/src/pages/AllVehicles.jsx
+++ frontend/src/pages/AllVehicles.jsx
@@ -36,8 +36,14 @@ function AllVehicles() {
         />
         <link rel="icon" type="image/png" href="../sec/assets/favicon.svg" />
       </Helmet>
-      <div>
-        <Nav />
+      <Nav />
+      <div
+        className="bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url(https://images-ext-1.discordapp.net/external/pPJ7tOzgHisfFsYFJFt5r5BzzqKZorId_JI9d3-5Q-M/https/i.pinimg.com/originals/94/1c/c3/941cc3a0ff66359276efa3e288aacb64.jpg?width=921&height=518)",
+        }}
+      >
         <VehicleList
           vehicleList={vehicleList}
           departure={departure}
